1e799a728036096aa534957b69674f8bdfcc2228
Cgs1208 <charangs157@gmail.com>
2023-12-14 13:50:01 +0530

    hover effect on movie cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ poster_path }) => {
   if (!poster_path) return null;
   return (
-    <div className="w-32 md:w-56 pr-2 md:pr-4">
+    <div className="w-32 md:w-56 pr-2 md:pr-4 hover:scale-110 transition-transform duration-200">
       <img src={IMG_CDN_URL + poster_path} alt="movie-card" />
     </div>
   );
